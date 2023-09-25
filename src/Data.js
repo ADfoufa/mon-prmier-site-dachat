29b595fda339409a1d842bf56b302cf3caf8621b
ADfoufa <benferrah.mouhamed@gmail.com>
2023-09-25 13:44:38 +0100

    challenge 11

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "ioytrhndcv",
+    name: "PS5",
+    price: 1000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "./image/R.png",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Magsafe",
+    price: 50,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "./image/MagSafe.webp",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Magsafe",
+    price: 299,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "./image/R1.png",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Samsung s23 Ultra",
+    price: 1299,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "./image/photo_2022-09-28_19-51-32.jpg",
+  },
+];
